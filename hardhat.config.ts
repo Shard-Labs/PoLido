bef681ee6fa7193bea62ef067e5e5c5810dd143a
GitMark0 <marko.ivankovic650@gmail.com>
2021-09-15 11:32:55 +0200

    add mocha timeout

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -50,9 +50,12 @@ const config: HardhatUserConfig = {
         }
     },
     typechain: {
-        outDir: "typechain",
-        target: "ethers-v5"
-    }
+        outDir: 'typechain',
+        target: 'ethers-v5',
+    },
+    mocha: {
+        timeout: 100000,
+    },
 };
 
 export default config;
