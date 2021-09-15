02a12c5effa5b59af3441471212e5f0ff6fffff8
idirall22 <idirall22@gmail.com>
2021-09-15 16:41:47 +0100

    add the polygon stake manager interface

---
+++ contracts/IStakeManager.sol
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: 2021 Shardlabs
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "hardhat/console.sol";
+
+interface IStakeManager {
+    /// @notice Stake a validator on polygon stake manager.
+    /// @param user user that own the validator in our case the validator contract.
+    /// @param amount amount to stake.
+    /// @param heimdallFee heimdall fees.
+    /// @param acceptDelegation accept delegation.
+    /// @param signerPubkey signer publickey used in heimdall node.
+    function stakeFor(
+        address user,
+        uint256 amount,
+        uint256 heimdallFee,
+        bool acceptDelegation,
+        bytes memory signerPubkey
+    ) external;
+
+    /// @notice Request unstake a validator.
+    /// @param validatorId validator id.
+    function unstake(uint256 validatorId) external;
+
+    /// @notice Increase the heimdall fees.
+    /// @param user user that own the validator in our case the validator contract.
+    /// @param heimdallFee heimdall fees.
+    function topUpForFee(address user, uint256 heimdallFee) external;
+
+    /// @notice Get the validator id using the user address.
+    /// @param user user that own the validator in our case the validator contract.
+    /// @return return the validator id
+    function getValidatorId(address user) external view returns (uint256);
+
+    /// @notice get the validator contract used for delegation.
+    /// @param validatorId validator id.
+    /// @return return the address of the validator contract.
+    function getValidatorContract(uint256 validatorId)
+        external
+        view
+        returns (address);
+
+    /// @notice Withdraw accumulated rewards
+    /// @param validatorId validator id.
+    function withdrawRewards(uint256 validatorId) external;
+}
