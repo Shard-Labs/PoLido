9521716c5c4199dfbb4d343d6fef94c96cba207f
GitMark0 <marko.ivankovic650@gmail.com>
2021-09-16 23:00:59 +0200

    update submit function with minting logic

--- contracts/LidoMatic.sol
+++ contracts/LidoMatic.sol
@@ -5,48 +5,42 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LidoMatic is ERC20("Staked Matic", "StMATIC") {
     /**
-     * CONSTANTS
-     * TODO: Extract constants to separate library
-     * TODO: Replace token addresses with Ethereum Mainnet ones, these are for Mumbai Testnet
+     * STATE VARS
      */
-    address public constant GOERLI_TEST_TOKEN =
-        0x7af963cF6D228E564e2A0aA0DdBF06210B38615D;
+    mapping(address => uint256) public userToShares;
+    // Value of totalDelegated needs to be updated periodically off chain because of slashing and rewarding
+    // It is calculated as the sum of delegated MATIC across all validatorShares
+    uint256 public totalDelegated;
+    // Value of totalBuffered needs to be set to 0 after the periodic update has been done
+    uint256 public totalBuffered;
+    // Address of Matic token
+    address public token;
 
-    /**
-     * @dev Send funds to the validator share contract with optional _referral parameter
-     * @notice Requires that msg.sender has approved _amount of MATIC to this contract
-     * @return Amount of StMATIC shares generated
-     * TODO: Add referral param
-     * TODO: Implement formula for minting StMATIC, now it returns 1:1 StMatic for MATIC
-     */
-    function buyVoucher(uint256 _amount) external returns (uint256) {
-        require(_amount > 0, "Invalid amount");
-
-        IERC20(GOERLI_TEST_TOKEN).transferFrom(
-            msg.sender,
-            address(this),
-            _amount
-        );
-
-        _mint(msg.sender, _amount);
-
-        return _amount;
+    constructor(address _token) {
+        token = _token;
     }
 
     /**
-     * @dev Send funds from the validator share contract to msg.sender with optional _referral parameter
-     * @notice Requires that msg.sender has approved _amount of StMATIC to this contract
-     * @return Amount of MATIC returned
-     * TODO: Add referral param
-     * TODO: Implement formula for retrieving MATIC, now it returns 1:1 MATIC for StMATIC
+     * @dev Send funds to LidoMatic contract and mint StMATIC to msg.sender
+     * @notice Requires that msg.sender has approved _amount of MATIC to this contract
+     * @param _amount - Amount of MATIC sent from msg.sender to this contract
+     * @return Amount of StMATIC shares generated
      */
-    function sellVoucher(uint256 _amount) external returns (uint256) {
+    function submit(uint256 _amount) external returns (uint256) {
         require(_amount > 0, "Invalid amount");
 
-        _burn(msg.sender, _amount);
+        IERC20(token).transferFrom(msg.sender, address(this), _amount);
 
-        IERC20(GOERLI_TEST_TOKEN).transfer(msg.sender, _amount);
+        uint256 totalShares = totalSupply();
+        uint256 totalPooledMatic = totalBuffered + totalDelegated;
+        uint256 amountToMint = totalShares != 0
+            ? (_amount * totalShares) / totalPooledMatic
+            : _amount;
 
-        return _amount;
+        _mint(msg.sender, amountToMint);
+
+        totalBuffered += _amount;
+
+        return amountToMint;
     }
 }
