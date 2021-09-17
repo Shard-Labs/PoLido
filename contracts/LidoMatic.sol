5b9e6b24b890381820eefa94768fd700ee34df21
GitMark0 <marko.ivankovic650@gmail.com>
2021-09-17 15:17:08 +0200

    add validatorShare delegate API

--- contracts/LidoMatic.sol
+++ contracts/LidoMatic.sol
@@ -3,10 +3,15 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import "./interfaces/IValidatorShare.sol";
+
 contract LidoMatic is ERC20("Staked Matic", "StMATIC") {
-    /**
-     * STATE VARS
-     */
+    ////////////////////////////////////////////////////////////////
+    /////                                                    //////
+    /////             ***State Variables***                  /////
+    /////                                                    ////
+    ////////////////////////////////////////////////////////////
+
     mapping(address => uint256) public userToShares;
     // Value of totalDelegated needs to be updated periodically off chain because of slashing and rewarding
     // It is calculated as the sum of delegated MATIC across all validatorShares
@@ -16,12 +21,15 @@ contract LidoMatic is ERC20("Staked Matic", "StMATIC") {
     // Address of Matic token
     address public token;
 
+    /**
+     * @param _token - Address of MATIC token on Ethereum Mainnet
+     */
     constructor(address _token) {
         token = _token;
     }
 
     /**
-     * @dev Send funds to LidoMatic contract and mint StMATIC to msg.sender
+     * @dev Send funds to LidoMatic contract and mints StMATIC to msg.sender
      * @notice Requires that msg.sender has approved _amount of MATIC to this contract
      * @param _amount - Amount of MATIC sent from msg.sender to this contract
      * @return Amount of StMATIC shares generated
@@ -42,5 +50,117 @@ contract LidoMatic is ERC20("Staked Matic", "StMATIC") {
         totalBuffered += _amount;
 
         return amountToMint;
+    }
+
+    ////////////////////////////////////////////////////////////////
+    /////                                                    //////
+    /////             ***ValidatorShare API***               /////
+    /////                                                    ////
+    ////////////////////////////////////////////////////////////
+    
+    /**
+     * @dev API for delegatet buying vouchers from validatorShare
+     * @param _validatorShare - Address of validatorShare contract
+     * @param _amount - Amount of MATIC to use for buying vouchers
+     * @param _minSharesToMint - Minimum of shares that is bought with _amount of MATIC
+     * @return Actual amount of MATIC used to buy voucher, might differ from _amount because of _minSharesToMint
+     */
+    function buyVoucher(
+        address _validatorShare,
+        uint256 _amount,
+        uint256 _minSharesToMint
+    ) public returns (bytes memory) {
+        (bool success, bytes memory data) = _validatorShare.delegatecall(
+            abi.encodeWithSignature(
+                "buyVoucher(uint256,uint256)",
+                _amount,
+                _minSharesToMint
+            )
+        );
+
+        require(success, "buyVoucher delegatecall failed");
+
+        return data;
+    }
+
+    /**
+     * @dev API for delegated restaking rewards to validatorShare
+     * @param _validatorShare - Address of validatorShare contract
+     */
+    function restake(address _validatorShare) public {
+        (bool success, ) = _validatorShare.delegatecall(
+            abi.encodeWithSignature("restake()")
+        );
+
+        require(success, "restake delegatecall failed");
+    }
+
+    /**
+     * @dev API for delegated unstaking and claiming tokens from validatorShare
+     * @param _validatorShare - Address of validatorShare contract
+     * @param _unbondNonce - TODO
+     */
+    function unstakeClaimTokens_new(
+        address _validatorShare,
+        uint256 _unbondNonce
+    ) public {
+        (bool success, ) = _validatorShare.delegatecall(
+            abi.encodeWithSignature(
+                "unstakeClaimTokens_new(uint256)",
+                _unbondNonce
+            )
+        );
+
+        require(success, "unstakeClaimTokens_new delegatecall failed");
+    }
+
+    /**
+     * @dev API for delegated selling vouchers from validatorShare
+     * @param _validatorShare - Address of validatorShare contract
+     * @param _claimAmount - Amount of MATIC to claim
+     * @param _maximumSharesToBurn - Maximum amount of shares to burn
+     */
+    function sellVoucher_new(
+        address _validatorShare,
+        uint256 _claimAmount,
+        uint256 _maximumSharesToBurn
+    ) public {
+        (bool success, ) = _validatorShare.delegatecall(
+            abi.encodeWithSignature(
+                "sellVoucher_new(uint256,uint256)",
+                _claimAmount,
+                _maximumSharesToBurn
+            )
+        );
+
+        require(success, "sellVoucher_new delegatecall failed");
+    }
+
+    /**
+     * @dev API for getting total stake of an user from validatorShare
+     * @param _validatorShare - Address of validatorShare contract
+     * @param _user - Address of user
+     * @return Total stake of _user and MATIC -> share exchange rate
+     */
+    function getTotalStake(IValidatorShare _validatorShare, address _user)
+        public
+        view
+        returns (uint256, uint256)
+    {
+        return _validatorShare.getTotalStake(_user);
+    }
+
+    /**
+     * @dev API for liquid rewards of an user from validatorShare
+     * @param _validatorShare - Address of validatorShare contract
+     * @param _user - Address of user
+     * @return Liquid rewards of _user
+     */
+    function getLiquidRewards(IValidatorShare _validatorShare, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        return _validatorShare.getLiquidRewards(_user);
     }
 }
