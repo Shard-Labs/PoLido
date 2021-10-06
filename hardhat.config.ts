908ac7cfa17ce286b4ba8d52f17390f4c98d5810
GitMark0 <marko.ivankovic650@gmail.com>
2021-10-06 13:07:13 +0200

    config etherscan verificator

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,64 +1,56 @@
-import * as dotenv from "dotenv";
-import * as path from "path";
-import { task, HardhatUserConfig } from "hardhat/config";
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import { HardhatUserConfig } from 'hardhat/config';
 
-import "@typechain/hardhat";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
-import "@openzeppelin/hardhat-upgrades";
-import "hardhat-gas-reporter";
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import '@openzeppelin/hardhat-upgrades';
+import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-gas-reporter';
 
-dotenv.config({ path: path.join(__dirname, ".env") });
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY;
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
-    defaultNetwork: "hardhat",
+    defaultNetwork: 'hardhat',
     solidity: {
-        version: "0.8.7",
+        version: '0.8.7',
         settings: {
             optimizer: {
                 enabled: true,
-                runs: 200
-            }
-        }
+                runs: 200,
+            },
+        },
     },
     networks: {
         localhost: {
-            url: "http://127.0.0.1:8545"
+            url: 'http://127.0.0.1:8545',
         },
         goerli: {
             url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
             accounts: [`0x${GOERLI_PRIVATE_KEY}`],
-            gasPrice: 10000000000
+            gasPrice: 10000000000,
         },
         mainnet: {
             url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-            accounts: [`0x${MAINNET_PRIVATE_KEY}`]
-        }
+            accounts: [`0x${MAINNET_PRIVATE_KEY}`],
+        },
     },
     typechain: {
-        outDir: "typechain",
-        target: "ethers-v5"
+        outDir: 'typechain',
+        target: 'ethers-v5',
     },
     mocha: {
-        timeout: 100000
-    }
+        timeout: 100000,
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
