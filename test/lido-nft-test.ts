4f92514e85a932f8730f5bf9816cf2ba109c9910
GitMark0 <marko.ivankovic650@gmail.com>
2021-11-10 14:36:44 +0100

    start every test fresh

--- test/lido-nft-test.ts
+++ test/lido-nft-test.ts
@@ -6,16 +6,18 @@ import { expect } from "chai";
 import { BigNumber } from "ethers";
 
 describe("LidoNFT", () => {
-    let deployer: SignerWithAddress;
+    let lido: SignerWithAddress;
     let accounts: SignerWithAddress[];
     let lidoNFT: LidoNFT;
 
     before(async () => {
-        [deployer, ...accounts] = await ethers.getSigners();
+        [lido, ...accounts] = await ethers.getSigners();
+    });
 
+    beforeEach(async () => {
         const LidoNft = (await ethers.getContractFactory(
             "LidoNFT",
-            deployer
+            lido
         )) as LidoNFT__factory;
 
         lidoNFT = (await upgrades.deployProxy(LidoNft, [
@@ -24,28 +26,30 @@ describe("LidoNFT", () => {
         ])) as LidoNFT;
         await lidoNFT.deployed();
 
-        await lidoNFT.setLido(deployer.address);
+        await lidoNFT.setLido(lido.address);
+
+        // Mint some tokens
+
+        await lidoNFT.mint(lido.address);
+        await lidoNFT.mint(accounts[0].address);
+        await lidoNFT.mint(accounts[0].address);
+        await lidoNFT.mint(accounts[1].address);
+        await lidoNFT.mint(accounts[1].address);
+        await lidoNFT.mint(accounts[1].address);
+        await lidoNFT.mint(accounts[2].address);
+        await lidoNFT.mint(accounts[2].address);
+        await lidoNFT.mint(accounts[2].address);
+        await lidoNFT.mint(accounts[2].address);
     });
 
     describe("Testing main functionalities...", async () => {
-        it("should successfully mint 1 token to deployer, 2 tokens to account0, 3 tokens to account1 and 4 tokens to account3", async () => {
-            await lidoNFT.mint(deployer.address);
-            await lidoNFT.mint(accounts[0].address);
-            await lidoNFT.mint(accounts[0].address);
-            await lidoNFT.mint(accounts[1].address);
-            await lidoNFT.mint(accounts[1].address);
-            await lidoNFT.mint(accounts[1].address);
-            await lidoNFT.mint(accounts[2].address);
-            await lidoNFT.mint(accounts[2].address);
-            await lidoNFT.mint(accounts[2].address);
-            await lidoNFT.mint(accounts[2].address);
-
-            const deployerBalance = await lidoNFT.balanceOf(deployer.address);
+        it("should successfully mint 1 token to lido, 2 tokens to account0, 3 tokens to account1 and 4 tokens to account3", async () => {
+            const deployerBalance = await lidoNFT.balanceOf(lido.address);
             const account0Balance = await lidoNFT.balanceOf(accounts[0].address);
             const account1Balance = await lidoNFT.balanceOf(accounts[1].address);
             const account2Balance = await lidoNFT.balanceOf(accounts[2].address);
 
-            const totalSupply = await lidoNFT.totalSupply();
+            const totalSupply = await lidoNFT.tokenIdIndex();
 
             expect(totalSupply.toNumber()).to.equal(10);
             expect(deployerBalance.toNumber()).to.equal(1);
@@ -54,7 +58,7 @@ describe("LidoNFT", () => {
             expect(account2Balance.toNumber()).to.equal(4);
         });
         it("Should successfully retrieve owned tokens of address", async () => {
-            const deployerTokens = await lidoNFT.getOwnedTokens(deployer.address);
+            const deployerTokens = await lidoNFT.getOwnedTokens(lido.address);
             const account0Tokens = await lidoNFT.getOwnedTokens(accounts[0].address);
             const account1Tokens = await lidoNFT.getOwnedTokens(accounts[1].address);
             const account2Tokens = await lidoNFT.getOwnedTokens(accounts[2].address);
@@ -69,20 +73,20 @@ describe("LidoNFT", () => {
             expect(account1Tokens).to.eql(account1Expected);
             expect(account2Tokens).to.eql(account2Expected);
         });
-        it("Should successfully transfer token 2 from account0 to deployer and update owned tokens", async () => {
+        it("Should successfully transfer token 2 from account0 to lido and update owned tokens", async () => {
             lidoNFT = lidoNFT.connect(accounts[0]);
-            await lidoNFT.approve(deployer.address, 2);
+            await lidoNFT.approve(lido.address, 2);
 
             // Check if a token is in approval list
-            const deployerApprovedPre = await lidoNFT.getApprovedTokens(deployer.address);
+            const deployerApprovedPre = await lidoNFT.getApprovedTokens(lido.address);
             const approvedExpectedPre = [BigNumber.from(2)];
             expect(deployerApprovedPre).to.eql(approvedExpectedPre);
 
-            lidoNFT = lidoNFT.connect(deployer);
-            await lidoNFT.transferFrom(accounts[0].address, deployer.address, 2);
+            lidoNFT = lidoNFT.connect(lido);
+            await lidoNFT.transferFrom(accounts[0].address, lido.address, 2);
 
             const deployerExpected = [BigNumber.from(1), BigNumber.from(2)];
-            const deployerTokens = await lidoNFT.getOwnedTokens(deployer.address);
+            const deployerTokens = await lidoNFT.getOwnedTokens(lido.address);
 
             const account0Expected = [BigNumber.from(3)];
             // Remove all 0 entries
@@ -92,7 +96,7 @@ describe("LidoNFT", () => {
             expect(account0Expected).to.eql(account0Tokens);
 
             // Check if approval was reset
-            const deployerApprovedPost = (await lidoNFT.getApprovedTokens(deployer.address)).filter(tokenId => tokenId.toNumber() !== 0);
+            const deployerApprovedPost = (await lidoNFT.getApprovedTokens(lido.address)).filter(tokenId => tokenId.toNumber() !== 0);
             expect(deployerApprovedPost.length).to.equal(0);
         });
     });
