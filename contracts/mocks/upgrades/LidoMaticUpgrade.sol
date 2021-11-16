a9d2bbec7ca5d8e3934ec6ad02c4dcd93e3d60cb
GitMark0 <marko.ivankovic650@gmail.com>
2021-11-16 12:56:42 +0100

    remove totalDelegated, replace all occurences with dynamic calculation

--- contracts/mocks/upgrades/LidoMaticUpgrade.sol
+++ contracts/mocks/upgrades/LidoMaticUpgrade.sol
@@ -1,26 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.7;
 
-
 import "../../LidoMatic.sol";
 
 contract LidoMaticUpgrade is LidoMatic {
-
     bool public constant upgraded = true;
-
-    /**
-     * @dev Used for testing purposes only
-     */
-    function simulateSlashing() external {
-        totalDelegated = totalSupply() / 2;
-        totalBuffered = 0;
-    }
-
-    /**
-     * @dev Used for testing purposes only
-     */
-    function simulateRewarding() external {
-        totalDelegated = totalSupply() * 2;
-        totalBuffered = 0;
-    }
 }
