5421ba1adc4437409765f18cd8e669a929059e67
idirall22 <idirall22@gmail.com>
2021-11-22 14:29:26 +0100

    fix validatorProxy contract bugs found during tests

--- contracts/interfaces/IValidatorProxy.sol
+++ contracts/interfaces/IValidatorProxy.sol
@@ -3,7 +3,11 @@
 pragma solidity 0.8.7;
 
 interface IValidatorProxy {
+    /// @notice Allows to set a new validator implementation.
+    /// @param _newImplementation new address.
     function setValidatorImplementation(address _newImplementation) external;
 
+    /// @notice Allows to set a new operator.
+    /// @param _newOperator new address.
     function setOperator(address _newOperator) external;
 }
