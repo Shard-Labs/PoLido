0464f002ffdd9655906f8e5428bc6c268227250c
GitMark0 <marko.ivankovic650@gmail.com>
2021-11-27 21:16:32 +0100

    abstract MockStakeManager from MockValidatorShare

--- contracts/mocks/MockValidatorShare.sol
+++ contracts/mocks/MockValidatorShare.sol
@@ -6,22 +6,29 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/IValidatorShare.sol";
 import "../interfaces/IStakeManager.sol";
 
+import "hardhat/console.sol";
+
 contract MockValidatorShare is IValidatorShare {
     address public token;
 
     uint256 public totalStaked;
+    uint256 public totalShares;
+    uint256 public override validatorId;
 
     mapping(address => uint256) public override unbondNonces;
+    mapping(address => mapping(uint256 => uint256)) public amount2Claim;
+    mapping(address => uint256) public user2Shares;
 
     IStakeManager stakeManager;
 
-    constructor(address _token, address _stakeManager) {
+    constructor(
+        address _token,
+        address _stakeManager,
+        uint256 _id
+    ) {
         token = _token;
         stakeManager = IStakeManager(_stakeManager);
-    }
-
-    function validatorId() public pure override returns (uint256) {
-        return 1;
+        validatorId = _id;
     }
 
     function withdrawRewards() external override {
@@ -49,28 +56,41 @@ contract MockValidatorShare is IValidatorShare {
         override
         returns (uint256)
     {
-        totalStaked += _amount;
+        user2Shares[msg.sender] += totalStaked != 0
+            ? (_amount * totalShares) / totalStaked
+            : _amount;
 
+        totalStaked += _amount;
+        totalShares += _amount;
         require(
-            stakeManager.delegationDeposit(validatorId(), _amount, msg.sender),
+            stakeManager.delegationDeposit(validatorId, _amount, msg.sender),
             "deposit failed"
         );
 
         return 1;
     }
 
-    function sellVoucher_new(uint256, uint256) external override {
+    function sellVoucher_new(uint256 _claimAmount, uint256) external override {
         uint256 unbondNonce = unbondNonces[msg.sender] + 1;
 
+        require(
+            _claimAmount <= user2Shares[msg.sender],
+            "Invalid amount to claim"
+        );
+
         unbondNonces[msg.sender] = unbondNonce;
+        amount2Claim[msg.sender][unbondNonce] = _claimAmount;
+        user2Shares[msg.sender] -= _claimAmount;
     }
 
-    function unstakeClaimTokens_new(uint256) external override {
-        stakeManager.unstakeClaim(validatorId());
-        IERC20(token).transfer(
-            msg.sender,
-            IERC20(token).balanceOf(address(this))
-        );
+    function unstakeClaimTokens_new(uint256 _unbondNonce) external override {
+        uint256 claimAmount = amount2Claim[msg.sender][_unbondNonce];
+        uint256 amount2Transfer = (claimAmount * totalStaked) / totalShares;
+        totalShares -= claimAmount;
+        totalStaked -= amount2Transfer;
+        //stakeManager.unstakeClaim(validatorId);
+        console.log("%s", IERC20(token).balanceOf(address(this)));
+        IERC20(token).transfer(msg.sender, amount2Transfer);
     }
 
     function getTotalStake(address)
