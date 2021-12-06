8bfc8164ad3a17252feafd090caf83206fd227e7
GitMark0 <marko.ivankovic650@gmail.com>
2021-12-06 18:37:54 +0100

    update deployment details

--- scripts/tasks.ts
+++ scripts/tasks.ts
@@ -1,26 +1,26 @@
-import { BigNumber } from "ethers";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { BigNumber } from 'ethers';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
 // In the future, select from different deployment details file based on the --network argument
 // For now it is hardcoded to use only Goerli
-import * as GOERLI_DEPLOYMENT_DETAILS from "../deploy-goerli.json";
-import { GoerliOverrides, TokenAddresses } from "./constants";
-import { attachContract } from "./utils";
+import * as GOERLI_DEPLOYMENT_DETAILS from '../deploy-goerli.json';
+import { GoerliOverrides, TokenAddresses } from './constants';
+import { attachContract } from './utils';
 
 const verifyContract = async (
     hre: HardhatRuntimeEnvironment,
     contractAddress: string
 ) => {
-    await hre.run("verify:verify", {
-        address: contractAddress
+    await hre.run('verify:verify', {
+        address: contractAddress,
     });
 };
 
 export const verify = async (hre: HardhatRuntimeEnvironment) => {
     const contracts = [
-        GOERLI_DEPLOYMENT_DETAILS.lido_matic_implementation,
+        GOERLI_DEPLOYMENT_DETAILS.stMATIC_implementation,
         GOERLI_DEPLOYMENT_DETAILS.node_operator_registry_implementation,
-        GOERLI_DEPLOYMENT_DETAILS.validator_factory_implementation
+        GOERLI_DEPLOYMENT_DETAILS.validator_factory_implementation,
     ];
 
     for (const contract of contracts) {
@@ -38,10 +38,19 @@ export const addOperator = async (
     const nodeOperatorRegistryAddress =
         GOERLI_DEPLOYMENT_DETAILS.node_operator_registry_proxy;
 
-    const nodeOperatorRegistry = await attachContract(hre, nodeOperatorRegistryAddress, "NodeOperatorRegistry");
+    const nodeOperatorRegistry = await attachContract(
+        hre,
+        nodeOperatorRegistryAddress,
+        'NodeOperatorRegistry'
+    );
 
     await (
-        await nodeOperatorRegistry.addOperator(name, rewardAddress, pubKey, GoerliOverrides)
+        await nodeOperatorRegistry.addOperator(
+            name,
+            rewardAddress,
+            pubKey,
+            GoerliOverrides
+        )
     ).wait();
 };
 
@@ -53,9 +62,15 @@ export const removeOperator = async (
     const nodeOperatorRegistryAddress =
         GOERLI_DEPLOYMENT_DETAILS.node_operator_registry_proxy;
 
-    const nodeOperatorRegistry = await attachContract(hre, nodeOperatorRegistryAddress, "NodeOperatorRegistry");
+    const nodeOperatorRegistry = await attachContract(
+        hre,
+        nodeOperatorRegistryAddress,
+        'NodeOperatorRegistry'
+    );
 
-    await (await nodeOperatorRegistry.removeOperator(id, GoerliOverrides)).wait();
+    await (
+        await nodeOperatorRegistry.removeOperator(id, GoerliOverrides)
+    ).wait();
 };
 
 export const stakeOperator = async (
@@ -67,42 +82,52 @@ export const stakeOperator = async (
     const nodeOperatorRegistryAddress =
         GOERLI_DEPLOYMENT_DETAILS.node_operator_registry_proxy;
 
-    const token = await attachContract(hre, TokenAddresses.Testv4, "ERC20", privateKey);
-    const nodeOperatorRegistry = await attachContract(hre, nodeOperatorRegistryAddress, "NodeOperatorRegistry", privateKey);
+    const token = await attachContract(
+        hre,
+        TokenAddresses.Testv4,
+        'ERC20',
+        privateKey
+    );
+    const nodeOperatorRegistry = await attachContract(
+        hre,
+        nodeOperatorRegistryAddress,
+        'NodeOperatorRegistry',
+        privateKey
+    );
     const operator = await nodeOperatorRegistry.getNodeOperator(1, false);
 
     await token.approve(operator[6], amount.add(heimdallFee));
     await (await nodeOperatorRegistry.stake(amount, heimdallFee)).wait();
 };
 
-export const unstakeOperator = async (
-    hre: HardhatRuntimeEnvironment
-) => {
+export const unstakeOperator = async (hre: HardhatRuntimeEnvironment) => {
     const nodeOperatorRegistryAddress =
         GOERLI_DEPLOYMENT_DETAILS.node_operator_registry_proxy;
 
-    const nodeOperatorRegistry = await attachContract(hre, nodeOperatorRegistryAddress, "NodeOperatorRegistry");
+    const nodeOperatorRegistry = await attachContract(
+        hre,
+        nodeOperatorRegistryAddress,
+        'NodeOperatorRegistry'
+    );
 
-    await (
-        await nodeOperatorRegistry.unstake()
-    ).wait();
+    await (await nodeOperatorRegistry.unstake()).wait();
 
-    console.log("unstakeOperator done");
+    console.log('unstakeOperator done');
 };
 
-export const claimUnstakeOperator = async (
-    hre: HardhatRuntimeEnvironment
-) => {
+export const claimUnstakeOperator = async (hre: HardhatRuntimeEnvironment) => {
     const nodeOperatorRegistryAddress =
         GOERLI_DEPLOYMENT_DETAILS.node_operator_registry_proxy;
 
-    const nodeOperatorRegistry = await attachContract(hre, nodeOperatorRegistryAddress, "NodeOperatorRegistry");
+    const nodeOperatorRegistry = await attachContract(
+        hre,
+        nodeOperatorRegistryAddress,
+        'NodeOperatorRegistry'
+    );
 
-    await (
-        await nodeOperatorRegistry.unstakeClaim()
-    ).wait();
+    await (await nodeOperatorRegistry.unstakeClaim()).wait();
 
-    console.log("claimUnstakeOperator done");
+    console.log('claimUnstakeOperator done');
 };
 
 export const getValidatorDetails = async (
@@ -113,36 +138,44 @@ export const getValidatorDetails = async (
         GOERLI_DEPLOYMENT_DETAILS.matic_stake_manager_proxy;
 
     if (validatorID === 0) {
-        console.log("validator id not valid");
+        console.log('validator id not valid');
     }
 
-    const stakeManagerArtifact = await hre.artifacts.readArtifact("IStakeManager");
-    const stakeManagerContract = await hre.ethers.getContractAt(stakeManagerArtifact.abi, stakeManagerAddress);
+    const stakeManagerArtifact = await hre.artifacts.readArtifact(
+        'IStakeManager'
+    );
+    const stakeManagerContract = await hre.ethers.getContractAt(
+        stakeManagerArtifact.abi,
+        stakeManagerAddress
+    );
 
     const v = await stakeManagerContract.validators(validatorID);
     const epoch = await stakeManagerContract.epoch();
     const withdrawalDelay = await stakeManagerContract.withdrawalDelay();
 
-    console.log("Validator:");
-    console.log("-----------------------------------------");
-    console.log("amount:", v.amount.toString());
-    console.log("activationEpoch:", v.activationEpoch.toString());
-    console.log("deactivationEpoch:", v.deactivationEpoch.toString());
-    console.log("reward:", v.reward.toString());
-    console.log("jailTime:", v.jailTime.toString());
-    console.log("signer:", v.signer.toString());
-    console.log("contractAddress:", v.contractAddress.toString());
-    console.log("status:", v.status.toString());
-    console.log("commissionRate:", v.commissionRate.toString());
-    console.log("lastCommissionUpdate:", v.lastCommissionUpdate.toString());
-    console.log("delegatorsReward:", v.delegatorsReward.toString());
-    console.log("delegatedAmount:", v.delegatedAmount.toString());
-    console.log("initialRewardPerStake:", v.initialRewardPerStake.toString());
+    console.log('Validator:');
+    console.log('-----------------------------------------');
+    console.log('amount:', v.amount.toString());
+    console.log('activationEpoch:', v.activationEpoch.toString());
+    console.log('deactivationEpoch:', v.deactivationEpoch.toString());
+    console.log('reward:', v.reward.toString());
+    console.log('jailTime:', v.jailTime.toString());
+    console.log('signer:', v.signer.toString());
+    console.log('contractAddress:', v.contractAddress.toString());
+    console.log('status:', v.status.toString());
+    console.log('commissionRate:', v.commissionRate.toString());
+    console.log('lastCommissionUpdate:', v.lastCommissionUpdate.toString());
+    console.log('delegatorsReward:', v.delegatorsReward.toString());
+    console.log('delegatedAmount:', v.delegatedAmount.toString());
+    console.log('initialRewardPerStake:', v.initialRewardPerStake.toString());
     console.log();
-    console.log("WithdrawalDelay:", withdrawalDelay.toString());
-    console.log("Current Epoch:", epoch.toString());
-    console.log("-----------------------------------------");
+    console.log('WithdrawalDelay:', withdrawalDelay.toString());
+    console.log('Current Epoch:', epoch.toString());
+    console.log('-----------------------------------------');
     if (v.deactivationEpoch !== hre.ethers.BigNumber.from(0)) {
-        console.log("Can claim unstaked tokens after:", v.deactivationEpoch.add(withdrawalDelay).toString());
+        console.log(
+            'Can claim unstaked tokens after:',
+            v.deactivationEpoch.add(withdrawalDelay).toString()
+        );
     }
 };
