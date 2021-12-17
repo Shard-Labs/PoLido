504312b7dbd4fdd7e4e46cab8c8d7ab4ef758e34
GitMark0 <marko.ivankovic650@gmail.com>
2021-12-17 10:48:26 +0100

    add state sync contracts

---
+++ contracts/interfaces/IFxStateChildTunnel.sol
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2021 ShardLabs
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+
+interface IFxStateChildTunnel {
+    function latestStateId() external view returns (uint256);
+
+    function latestRootMessageSender() external view returns (address);
+
+    function latestData() external view returns (bytes memory);
+
+    function sendMessageToRoot(bytes memory message) external;
+
+    function setFxRootTunnel(address _fxRootTunnel) external;
+}
--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 
 export const TokenAddresses = {
-    Testv4: "0x499d11e0b6eac7c0593d8fb292dcbbf815fb29ae",
+    Testv4: "0x499d11e0b6eac7c0593d8fb292dcbbf815fb29ae"
 };
 
 export const GoerliOverrides = {
