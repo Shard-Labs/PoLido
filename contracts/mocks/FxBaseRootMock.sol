960782978898426bf99db62d51728bf902dc43c8
GitMark0 <marko.ivankovic650@gmail.com>
2021-12-20 08:25:05 +0100

    add FxBaseRootMock

---
+++ contracts/mocks/FxBaseRootMock.sol
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2021 ShardLabs
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+
+contract FxBaseRootMock {
+    function sendMessageToChild(bytes memory _message) external {}
+}
