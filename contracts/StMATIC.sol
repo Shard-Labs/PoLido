bd453529f12fa5afd510898a90281ef1acc95b74
idirall22 <idirall22@gmail.com>
2022-01-21 07:30:53 +0100

    fix warning 2

--- contracts/StMATIC.sol
+++ contracts/StMATIC.sol
@@ -381,8 +381,20 @@ contract StMATIC is
         Operator.OperatorInfo[] memory operatorInfos = nodeOperatorRegistry
             .getOperatorInfos(true);
 
-        for (uint256 i = 0; i < operatorInfos.length; i++) {
-            IValidatorShare(operatorInfos[i].validatorShare).withdrawRewards();
+        uint256 operatorInfosLength = operatorInfos.length;
+
+        for (uint256 i = 0; i < operatorInfosLength; i++) {
+            IValidatorShare validatorShare = IValidatorShare(
+                operatorInfos[i].validatorShare
+            );
+            uint256 stMaticReward = validatorShare.getLiquidRewards(
+                address(this)
+            );
+            uint256 rewardThreshold = validatorShare.minAmount();
+
+            if (stMaticReward < rewardThreshold) {
+                validatorShare.withdrawRewards();
+            }
         }
 
         uint256 totalRewards = (
